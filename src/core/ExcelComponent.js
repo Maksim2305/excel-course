6636194f744c3ad3788c5449b104d386e5e83fd9
mzhurin <mzhurin@phoenixit.ru>
2022-12-02 10:43:05 +0300

    add some logics to table

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,17 +3,32 @@ import { DomListener } from "./DomListener";
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
-    this.name = options.name || ""
+    this.name = options.name || "";
+    this.emitter = options.emitter;
+    this.unsunscribers = [];
+    this.prepare();
   }
+  prepare() {}
 
   toHTML() {
     return "";
+  }
+  // добавляем слушатель
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  // подписываемся
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsunscribers.push(unsub);
   }
 
   init() {
     this.initDOMListeners();
   }
+  // удаляем события
   destroy() {
     this.removeDOMListeners();
+    this.unsunscribers.forEach((unsub) => unsub());
   }
 }
